b44b05f2fccf4c4e3ab53375a93c32210f242af0
Artem-Potapov <potapov23500@gmail.com>
2020-11-22 19:49:23 +0300

    Add search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 from flask_cors import CORS
+from flask import request
 
 file = open('books.json', 'r', encoding='utf-8')
 app = Flask(__name__)
@@ -8,14 +9,22 @@ CORS(app)
 books = json.load(file)
 
 
-@app.route('/')
-def hello_world():
-    return json.dumps(books)
+@app.route('/books')
+def give_books():
+    filters = json.loads(request.args.get('filters', '{}'))
+    print(filters)
+    _books = dict(books)
+    return _books
 
 
 @app.route('/authors')
 def come():
     return json.dumps([book['author'] for book in books])
+
+
+@app.route('/test')
+def test():
+    return request.args.get('data', 'ke5k')
 
 
 app.run()
