05cf42bbe3c4a75ef3f85308f42bfbc22d1ee98c
Artem-Potapov <potapov23500@gmail.com>
2020-12-20 20:04:08 +0300

    add sort for age

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 CORS(app)
 books = json.load(file)
 
+
 @app.route('/books')
 def give_books():
     filters = json.loads(request.args.get('filters', '{}'))
@@ -15,7 +16,8 @@ def give_books():
 
     if filters.get('author'):
         _books = list(filter(lambda book: book.get('author') == filters.get('author'), _books))
-
+    if filters.get('genre'):
+        _books = list(filter(lambda book: book.get('genre') == filters.get('genre'), _books))
     return json.dumps(_books)
 
 
@@ -27,9 +29,28 @@ def come():
     return json.dumps(list(authors))
 
 
-@app.route('/test')
-def test():
-    return request.args.get('data', 'ke5k')
+@app.route('/genres')
+def give_genre():
+    genre = set()
+    for book in books:
+        genre.add(book['genre'])
+    return json.dumps(list(genre))
+
+
+@app.route('/ages')
+def give_ages():
+    ages = set()
+    for book in books:
+        ages.add(book['age'])
+    return json.dumps(list(ages))
+
+
+@app.route('/rates_from5')
+def give_rate_from5():
+    ageper5 = set()
+    for book in books:
+        ageper5.add(book['rate_f5'])
+    return json.dumps(list(ageper5))
 
 
 app.run()
